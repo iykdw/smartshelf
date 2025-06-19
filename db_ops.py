fe0526b46b7221dceaccb78fb78d28ff1b0dc84d
mechanical-girl <struan@duncan-wilson.co.uk>
2025-06-19 21:02:54 +0100

    Fix typing

--- db_ops.py
+++ db_ops.py
@@ -1,7 +1,7 @@
 import sqlite3
 from contextlib import closing
+from typing import Any
 from typing import List
-from typing import Optional
 
 class DB:
     def __init__(self, persist, file, logging):
@@ -12,7 +12,7 @@ class DB:
     def log_trace(self, statement):
         self.logging.info(statement)
 
-    def _db_execute(self, command: str, args: tuple, com_type: int) -> Optional[List]:
+    def _db_execute(self, command: str, args: tuple, com_type: int) -> List:
         with closing(sqlite3.connect(f"{self.persist}/{self.file}")) as connection:
             connection.set_trace_callback(self.log_trace)
             with closing(connection.cursor()) as c:
@@ -22,19 +22,25 @@ class DB:
                     return c.execute(command, args).fetchone()
                 else:
                     c.execute(command, args)
+                    connection.commit()
+                    return []
 
-                connection.commit()
 
-
-    def execute(self, command: str, args: tuple = ()) -> None:
+    def execute(self, command: str, args: tuple[Any] | None = None) -> None:
+        if not args:
+            args = tuple()
         self.logging.debug(command)
         self.logging.debug(args)
         self._db_execute(command, args, 0)
 
 
-    def fetchone(self, command: str, args: tuple = ()) -> List | None:
+    def fetchone(self, command: str, args: tuple[Any] | None = None) -> List:
+        if not args:
+            args = tuple()
         result = self._db_execute(command, args, 1)
         return result
 
-    def fetchall(self, command: str, args: tuple = ()) -> List | None:
+    def fetchall(self, command: str, args: tuple[Any] | None = None) -> List:
+        if not args:
+            args = tuple()
         return self._db_execute(command, args, 2)
