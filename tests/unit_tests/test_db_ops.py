c23aa8d09651514a0a8a5279825092bd0eb98f02
iykdw <yasha@duncan-wilson.co.uk>
2026-08-22 19:29:46 +0100

    Add fixture

--- tests/unit_tests/test_db_ops.py
+++ tests/unit_tests/test_db_ops.py
@@ -1,19 +1,20 @@
 import logging
 import os
-import subprocess
 from pathlib import Path
 
 import pytest
 
 import smartshelf_logger
+from rooms_to_db import rooms_to_db
 from src.db_ops import DB as db_ops_DB
 
 cwd = os.getcwd()
 
 test_dir = Path(cwd) / "tests"
-test_db = Path(test_dir) / "test.db"
+db_file = Path(test_dir) / "test.db"
 test_logfile = Path(test_dir) / "test.log"
 schemafile = Path(cwd) / "table_schema.sql"
+rooms_file = Path(test_dir) / "rooms.json"
 
 log_files = [test_logfile.resolve()]
 logger = smartshelf_logger.get_logger(log_files, [], logging.DEBUG, __file__)
@@ -26,16 +27,15 @@ def DB():
 
     from src.main import _has_config, format_book_for_db_insertion, get_data_from_gb, get_rooms, suggest_position
 
-    DB_handler = db_ops_DB(test_db, logger)
+    DB_handler = db_ops_DB(db_file.resolve(), logger)
 
     if _has_config(DB_handler):
         logger.info("DB exists and is set up correctly!")
         return DB_handler
 
     logger.info("Creating DB now...")
-    command = [f'uv run sqlite3 {test_db.resolve()} ".read {schemafile.resolve()}"']
-    logger.info(command)
-    subprocess.run(command)
+    os.system(f'uv run sqlite3 {db_file.resolve()} ".read {schemafile.resolve()}"')
+    rooms_to_db(DB, rooms_file.resolve())
     logger.info(" done")
     with open("tests/isbns.json") as f:
         isbns = json.loads(f.read())
@@ -54,7 +54,3 @@ def DB():
                 )
 
     return DB_handler
-
-
-def test_idk(DB):
-    logger.info(os.getcwd())
